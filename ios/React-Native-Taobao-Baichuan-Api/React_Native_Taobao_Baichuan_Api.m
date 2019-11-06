dfac7d7128691547b4bb84934ef9ebc9d19fc4a0
xiewang <1010658096@qq.com>
2019-11-06 20:49:07 +0800

    upgrade ios

--- ios/React-Native-Taobao-Baichuan-Api/React_Native_Taobao_Baichuan_Api.m
+++ ios/React-Native-Taobao-Baichuan-Api/React_Native_Taobao_Baichuan_Api.m
@@ -13,12 +13,77 @@
 
 #import <AlibabaAuthSDK/ALBBSession.h>
 #import <AlibabaAuthSDK/ALBBUser.h>
+#import "ALiTradeWebViewController.h"
 
 @implementation React_Native_Taobao_Baichuan_Api
 
 
 RCT_EXPORT_MODULE(React_Native_Taobao_Baichuan_Api);
 
+RCT_EXPORT_METHOD(jumpByUrl:(NSString *)url callback:(RCTResponseSenderBlock)callback)
+{
+    //淘客信息
+    AlibcTradeTaokeParams *taoKeParams=[[AlibcTradeTaokeParams alloc] init];
+    taoKeParams.pid=  @"mm_120032403_0_0";
+    //打开方式
+    AlibcTradeShowParams* showParam = [[AlibcTradeShowParams alloc] init];
+    showParam.openType = AlibcOpenTypeAuto;
+    
+    showParam.isNeedPush = NO;
+    
+    ALiTradeWebViewController* view = [[ALiTradeWebViewController alloc] init];
+    
+    
+    NSInteger ret = [[AlibcTradeSDK sharedInstance].tradeService
+                     openByUrl: url
+                     identity:@"trade"
+                     webView: view.webView
+                     parentController: view
+                     showParams: showParam
+                     taoKeParams: taoKeParams
+                     trackParam:nil
+                     tradeProcessSuccessCallback:^(AlibcTradeResult * __nullable result) {
+                         if (result.result == AlibcTradeResultTypePaySuccess) {
+                             NSDictionary *res = @{@"result": @"success", @"orders": result.payResult.paySuccessOrders};
+                             callback(@[[NSNull null], res]);
+                         } else {
+                             callback(@[[NSNull null], [NSNull null]]);
+                         }
+                         
+                         NSLog(@"%@", result);
+                     }
+                     tradeProcessFailedCallback:^(NSError * __nullable error) {
+                         callback(@[[NSNull null],[NSNull null]]);
+                         
+                         NSLog(@"%@", error);
+                     }
+                     ];
+    //返回1,说明h5打开,否则不应该展示页面
+    if (ret == 1) {
+        //        [self.navigationController pushViewController:view animated:YES];
+    }
+    
+    [[ALBBSDK sharedInstance] setLoginResultHandler:^(ALBBSession *session) {
+        if([session isLogin]){//登录成功
+            ALBBUser *user ;
+            user = [[ALBBSession sharedInstance] getUser];
+            NSMutableDictionary *retUser = [NSMutableDictionary dictionaryWithCapacity:3];
+            [retUser setObject:user.nick forKey:@"nick"];
+            [retUser setObject:user.openId forKey:@"openId"];
+            [retUser setObject:user.openSid forKey:@"openSid"];
+            
+            [self sendEventWithName:@"EventReminder" body:@{@"user": retUser}];
+            
+            NSLog(@"用户login");
+        }else{//已登录变为未登录
+            NSLog(@"用户logout");
+        }
+        [self sendEventWithName:@"backFromTB" body:@"true"];
+    }];
+    
+    return ;
+    
+}
 
 RCT_EXPORT_METHOD(jump:(NSString *)itemId :(NSString *) orderId type:(NSString *)type callback:(RCTResponseSenderBlock)callback)
 {
@@ -37,29 +102,29 @@ RCT_EXPORT_METHOD(jump:(NSString *)itemId :(NSString *) orderId type:(NSString *
     id<AlibcTradePage> page = [AlibcTradePageFactory itemDetailPage: itemID];
     //淘客信息
     AlibcTradeTaokeParams *taoKeParams=[[AlibcTradeTaokeParams alloc] init];
-    taoKeParams.pid= nil;
+    taoKeParams.pid=  @"mm_120032403_0_0";;
     //打开方式
     AlibcTradeShowParams* showParam = [[AlibcTradeShowParams alloc] init];
     if([type isEqual:@"tmall"] ){
         showParam.openType = AlibcOpenTypeNative;
     } else {
-        showParam.openType = AlibcOpenTypeH5;
+        showParam.openType = AlibcOpenTypeAuto;
     }
     
     showParam.isNeedPush = NO;
     
-    //UIViewController *rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
-    AppDelegate *share = (AppDelegate *)[UIApplication sharedApplication].delegate;
-    UINavigationController *nav = (UINavigationController *) share.window.rootViewController;
-    
+    ALiTradeWebViewController* view = [[ALiTradeWebViewController alloc] init];
+
     //打开我的订单页
     id<AlibcTradePage> pageOrder = [AlibcTradePageFactory myOrdersPage:orderID isAllOrder:NO];
     
     NSInteger ret = [[AlibcTradeSDK sharedInstance].tradeService
-                     show: nav
+                     openByBizCode: @"detail"
                      page: [item  isEqual: @""]? pageOrder:page
+                     webView: view.webView
+                     parentController: view
                      showParams: showParam
-                     taoKeParams: nil
+                     taoKeParams: taoKeParams
                      trackParam:nil
                      tradeProcessSuccessCallback:^(AlibcTradeResult * __nullable result) {
                          if (result.result == AlibcTradeResultTypePaySuccess) {
